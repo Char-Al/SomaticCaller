972da0105651eca5b43460714a57d0f833b31e81
Char-Al <charles.van.goethem@gmail.com>
2017-10-11 17:03:01 +0200

    Defined options, add all steps, not working just print cmd

--- muse/bashPipeline.sh
+++ muse/bashPipeline.sh
@@ -4,13 +4,29 @@
 # [Charles VAN GOETHEM] SomaticCaller
 #          Pipeline base on GATK best practices
 # ------------------------------------------------------------------
-VERSION=0.1.0
+VERSION=0.0.1
 USAGE="Usage:	bashPipeline.sh [-h] [-l 6] -t <sample_tumor.bam> -n <sample_normal.bam> -v <sample_normal.vcf> -p <panelOfNormal.list>"
 PWD_PROJECT=$(pwd)
 
 COMMAND_LINE="${0} ${@}"
 CPT_ERROR=0
 CPT_WARNING=0
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+# Set magic variables for current file, directory, os, etc.
+__dir="$(cd "$(dirname "${BASH_SOURCE[${__b3bp_tmp_source_idx:-0}]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[${__b3bp_tmp_source_idx:-0}]}")"
+__base="$(basename "${__file}" .sh)"
 
 ################################################################################
 ################################################################################
@@ -108,7 +124,12 @@ tumor=""
 normal=""
 panel=()
 vcfNormal=""
+output=""
+
 LOG_LEVEL=6
+NB_THREAD=1
+MAX_RAM=16
+NB_CORE=1
 
 # Parse command line
 _arg_ctrl () {
@@ -133,7 +154,7 @@ _arg_unique () {
 	fi
 }
 
-while [ "$1" != "" ]; do
+while [[ "${@}" != "" ]]; do
 	case $1 in
 		-t | --tumor )			_arg_unique "-t (--tumor)" "${tumor}"
 								_arg_ctrl "-t (--tumor)" "${2}"
@@ -154,10 +175,33 @@ while [ "$1" != "" ]; do
 								shift
 								vcfNormal=$1
 								;;
+		-o | --output )			_arg_unique "-o (--output)" "${output}"
+								_arg_ctrl "-o (--output)" "${2}"
+								shift
+								output=$1
+								;;
 		-l | --log-level )		_arg_ctrl "-l (--log-level)" "${2}"
 								[ -z "${2##*[!0-9]*}" ] && error "Option -l (--log-level) need integer as input. (default 6)" && usage && exit 1
 								shift
 								LOG_LEVEL=$1
+								;;
+		-nt | --nb-thread )		_arg_ctrl "-nt (--nb-thread)" "${2}"
+								[ -z "${2##*[!0-9]*}" ] && error "Option -nt ( --nb-thread) need integer as input. (default 1)" && usage && exit 1
+								shift
+								NB_THREAD=$1
+								;;
+		-m | --max-ram )		_arg_ctrl "-m (--max-ram)" "${2}"
+								[ -z "${2##*[!0-9]*}" ] && error "Option -m (--max-ram) need integer as input. (default 1)" && usage && exit 1
+								shift
+								MAX_RAM=$1
+								;;
+		-c | --nb-core )		_arg_ctrl "-c (--nb-core)" "${2}"
+								[ -z "${2##*[!0-9]*}" ] && error "Option -c (--nb-core) need integer as input. (default 1)" && usage && exit 1
+								shift
+								NB_CORE=$1
+								;;
+		-d | --debug )			set -o xtrace
+								LOG_LEVEL="7"
 								;;
 		-h | --help )			usage
 								exit
@@ -172,31 +216,35 @@ done
 
 info "Command line : '$COMMAND_LINE'"
 
-
 ################################################################################
 ################################################################################
 # check if args are valid
 
 _contains_element () {
-	local e match="$1"
+	debug "Function : _contains_element ${@}"
+
+	local e="${1}"
 	shift
-	for e
+
+	for m
 	do
-		[[ "$e" == "$match" ]] && return 0;
+		[[ "$e" == "$m" ]] && echo 0 && return 0;
 	done
+
+	echo 1
 	return 1
 }
 
 _check_file () {
 	# Check if file is valid and extension (option)
 	# usage : _check_file $file [$extension]
-	debug "Check file '${1}'"
+	debug "Function : _check_file ${@}"
 
 	local filepath="${1}"
 	shift
 
 	local authorized_ext=()
-	while [ "$1" != "" ]; do
+	while [[ "${@}" != "" ]]; do
 		authorized_ext+=("${1}")
 		shift
 	done
@@ -204,20 +252,14 @@ _check_file () {
 	local filename=$(basename "${filepath}")
 	local extension="${filename##*.}"
 
-	debug "	file : ${filepath}"
-	debug "	filename : ${filename}"
-	debug "	extension : ${extension}"
-
 
 	if [[ ! -f ${filepath} ]]
 	then
 		error "File '${filepath}' is not a regular file."
 		CPT_ERROR=$[$CPT_ERROR +1]
-	elif [[ ! -z ${authorized_ext} ]]
+	elif [[ ! -z ${authorized_ext[@]} ]]
 	then
-		debug "	authorized extension : ${authorized_ext[@]}"
-		_contains_element "${extension}" "${authorized_ext[@]}"
-		local ext=$?
+		local ext=$(_contains_element "${extension}" "${authorized_ext[@]}")
 
 		if [[ ${ext} == 1 ]]
 		then
@@ -225,6 +267,27 @@ _check_file () {
 		fi
 	fi
 }
+
+_check_output () {
+	# Check if file is valid and extension (option)
+	# usage : _check_file $file [$extension]
+	debug "Function : _check_output ${@}"
+
+	local out="${1}"
+
+	if [[ -e ${out} ]]
+	then
+		error "Output '${out}' exists. Please make sure that repertory '${out}' is not exist."
+		CPT_ERROR=$[$CPT_ERROR +1]
+	else
+		mkdir "${out}"
+	fi
+
+}
+
+info "Number of thread : ${NB_THREAD}"
+info "Max ram : ${MAX_RAM}G"
+info "Number of core : ${NB_CORE}"
 
 # Argument tumor
 if [[ $tumor == "" ]]
@@ -256,7 +319,6 @@ else
 	_check_file ${vcfNormal} "vcf"
 fi
 
-
 # Argument panelOfNormal
 if [[ ${#panel[@]} -eq 0 ]]
 then
@@ -266,9 +328,19 @@ else
 	info "${#panel[@]} files uses for panel of normal :"
 	for p in ${panel[@]}
 	do
-		info "	$normal"
+		info "	${p}"
 		_check_file ${p} "bam" "sam" "cram"
 	done
+fi
+
+# Argument output
+if [[ $output == "" ]]
+then
+	error "Option -o (--output) is mandatory"
+	CPT_ERROR=$[$CPT_ERROR +1]
+else
+	info "Output Repertory : $vcfNormal"
+	_check_output ${output}
 fi
 
 if [[ CPT_ERROR -gt 0 ]]
@@ -277,3 +349,111 @@ then
 	usage
 	exit 1
 fi
+
+################################################################################
+################################################################################
+
+info "__file: ${__file}"
+info "__dir: ${__dir}"
+info "__base: ${__base}"
+info "OSTYPE: ${OSTYPE}"
+
+QUEUE="/home/vangoethemc/work-mobidic/nenufaar/software/Queue/3.6/Queue.jar"
+GATK="/home/vangoethemc/work-mobidic/nenufaar/software/GenomeAnalysisTK/3.6.0/GenomeAnalysisTK.jar"
+PICARD="/home/vangoethemc/work-mobidic/nenufaar/software/picard/2.6.0/picard.jar"
+
+HAPMAP="/home/vangoethemc/work-mobidic/nenufaar/refData/HapMap/hapmap_3.3_hg19_pop_stratified_af.vcf.gz"
+COSMIC="/home/vangoethemc/work-mobidic/nenufaar/refData/COSMIC/CosmicCodingMuts_withCHR.vcf"
+REF="/home/vangoethemc/work-mobidic/nenufaar/refData/genome/hg19/hg19.fa"
+DBSNP="/home/vangoethemc/work-mobidic/nenufaar/refData/dbSNP/138/CORRECT_dbsnp_138.hg19.vcf"
+
+export JAVA_HOME=/nfs/work/mobidic/nenufaar/software/jre1.8.0_111/bin/
+JAVA=/nfs/work/mobidic/nenufaar/software/jre1.8.0_111/bin/java
+export PATH=${JAVA_HOME}:${PATH}
+export LD_LIBRARY_PATH="/trinity/shared/apps/local/slurm-drmaa-1.0.7/lib/:${LD_LIBRARY_PATH:-}"
+QUEUE_RUNNER="-jobRunner Drmaa"
+
+debug "${panel[@]}"
+
+for e in ${panel[@]}
+do
+	CURRENT_SAMPLE_BASEDIR_NAME=$(basename "${e}")
+	CURRENT_SAMPLE_NAME=${CURRENT_SAMPLE_BASEDIR_NAME%.bam}
+	info "BASENAME CURRENT SAMPLE : ${CURRENT_SAMPLE_NAME}"
+	cmd="$JAVA -jar -Djava.io.tmpdir=out/tmp_PoN/${CURRENT_SAMPLE_NAME} -Xmx${MAX_RAM}g $QUEUE -l WARN \
+	    -S queueScripts/muTect2_PoN.scala \
+	    -tumor $p \
+	    -dbsnp $DBSNP \
+	    -o $CURRENT_SAMPLE_NAME.1_normal.vcf.gz \
+	    -R $REF \
+	    ${QUEUE_RUNNER} \
+	    -jobSGDir out/ \
+	    -run"
+	info "Make PoN : ${cmd}"
+	info "echo \"$CURRENT_SAMPLE_NAME.1_normal.vcf.gz\" >> PoN.list"
+done
+
+cmd="srun --job-name=splitVCF -N=1 -n=1 -c=${NB_THREAD} --partition=defq --account=IURC $JAVA -jar -Xmx${MAX_RAM}g $GATK \
+	-T CombineVariants \
+	-nt ${NB_THREAD} \
+	--arg_file PoN.list \
+	-minN 2 \
+	--setKey \"null\" \
+	--filteredAreUncalled \
+	--filteredrecordsmergetype KEEP_IF_ANY_UNFILTERED \
+	-o 2_pon_combinevariants.vcf.gz \
+	-R $REF \
+	--genotypemergeoption UNIQUIFY"
+info "Merge Variants : ${cmd}"
+
+cmd="srun --job-name=splitVCF -N1 -n1 -c24 --partition=defq --account=IURC $JAVA -jar $PICARD MakeSitesOnlyVcf \
+	I=2_pon_combinevariants.vcf.gz \
+	O=3_pon_siteonly.vcf.gz"
+info "PoN site only : ${cmd}"
+
+cmd="$JAVA -jar -Djava.io.tmpdir=out/tmp_contEst -Xmx${MAX_RAM}g $QUEUE -S queueScripts/contEst.scala \
+	-eval $tumor \
+	-genotypes $vcfNormal \
+	-popfile $HAPMAP \
+	-o 4_T_contest.txt \
+	-R $REF \
+	${QUEUE_RUNNER} \
+	-jobSGDir out/ \
+	-run"
+info "Sample tumor contamination estimation : ${cmd}"
+
+cmd="$JAVA -jar -Djava.io.tmpdir=out/tmp_contEst -Xmx${MAX_RAM}g $QUEUE -S queueScripts/contEst.scala \
+	-eval $normal \
+	-genotypes $vcfNormal \
+	-popfile $HAPMAP \
+	-o 5_N_contest.txt \
+	-R $REF \
+	${QUEUE_RUNNER} \
+	-jobSGDir out/ \
+	-run "
+info "Sample normal contamination estimation : ${cmd}"
+
+cmd="srun --job-name=ArtMetricsTumor -N1 -n1 -c24 --partition=defq --account=IURC $JAVA -jar $PICARD CollectSequencingArtifactMetrics \
+	I=$tumor \
+	O=6_T_artifact \
+	R=$REF"
+info "Collect Sequencing Artifact Metrics tumor : ${cmd}"
+
+cmd="srun --job-name=ArtMetricsNormal -N1 -n1 -c24 --partition=defq --account=IURC $JAVA -jar $PICARD CollectSequencingArtifactMetrics \
+	I=$normal \
+	O=7_N_artifact \
+	R=$REF"
+info "Collect Sequencing Artifact Metrics normal : ${cmd}"
+
+cmd="$JAVA -Djava.io.tmpdir=out/tmpp_call/ -Xmx${MAX_RAM}g -jar $QUEUE -S queueScripts/muTect2_calling.scala \
+	-tumor $tumor \
+	-normal $normal \
+	-dbsnp $DBSNP \
+	-cosmic $COSMIC \
+	-normal_panel 2_pon_combinevariants.vcf.gz \
+	-o 8_mutect2.vcf.gz \
+	-R $REF  \
+	${QUEUE_RUNNER} \
+	-jobSGDir out/ \
+	-run "
+info "Calling by MuTect2 : ${cmd}"
